27242fbdddd807d733c13b4d4704f0700a877033
Oluwabukola Arowolo <arowolooluwabukola14@gmail.com>
2025-03-21 22:48:40 +0100

    Update Advanced String Functions, Regex and Clause.sql

--- Advanced String Functions, Regex and Clause.sql
+++ Advanced String Functions, Regex and Clause.sql
@@ -17,3 +17,20 @@ and p1.id < p2.id
 select max(salary) secondhighestsalary
 from employee
 where salary not in (select max(salary) from employee)
+
+-- 1484. Group Sold Products By The Date
+select sell_date, count(distinct product) as num_sold, 
+group_concat(distinct product order by product asc) products from activities
+group by sell_date
+order by sell_date
+
+-- 1327. List the Products Ordered in a Period
+select p.product_name, sum(o.unit) unit from products p
+join orders o on p.product_id = o.product_id
+where o.order_date >= '2020-02-01' and o.order_date < '2020-03-01'
+group by p.product_name
+having unit >=100
+
+-- 1517. Find Users With Valid E-Mails
+select user_id, name, mail from users
+where mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$'
